00d4394f3b6b1787c60c4d7f86d43eb057ca4e4e
rahimz <rahim.aghareb@gmail.com>
2018-11-22 12:04:19 +0330

    admin models are created and published

--- calculates/admin.py
+++ calculates/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Part,Panel,Band, Project
+
+
+
+@admin.register(Part)
+class PartAdmin(admin.ModelAdmin):
+    list_display = ('length', 'width', 'quantity', 'panel', 'project')
+    ordering = ('created',)
+
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ('author', 'name', 'created')
+    ordering = ('author',)
+
+@admin.register(Panel)
+class PanelAdmin(admin.ModelAdmin):
+    list_display = ('name', 'length', 'width')
+    ordering = ('name',)
+
+@admin.register(Band)
+class BandAdmin(admin.ModelAdmin):
+    list_display = ('name', 'band')
+    oredering =('name',)
